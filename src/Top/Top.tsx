2207e42439cb6728f6d262f9273911d4338e94a1
ShunKUN1182 <2250378@ecc.ac.jp>
2026-04-24 19:46:08 +0900

    コンポーネントが出来ました

--- src/Top/Top.tsx
+++ src/Top/Top.tsx
@@ -1,15 +1,20 @@
 import "./Top.css";
 import Hituji from "../assets/hituzi.png";
 import { Link } from "@tanstack/react-router";
+import SelectBtn from "../components/SelectBtn";
 
 function Top() {
     return (
         <>
-            <div>
-                <p className="head_msg">1日お疲れ様じゃ</p>
+            <div className="wrapper">
+                <section className="head_msg">
+                    <h2>1日お疲れ様じゃ</h2>
+                </section>
                 <img src={Hituji} alt="" />
-                <Link to="/Diagnosis">診断するかい？</Link>
-                <br />
+                <Link to="/Diagnosis">
+                    <SelectBtn text="診断で選ぶ" imgSrc={Hituji} />
+                </Link>
+
                 <Link to="/Choice">選ぶこともできんでー</Link>
             </div>
         </>
